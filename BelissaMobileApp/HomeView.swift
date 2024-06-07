18556e6ed785849e2332dbac0808f48dc20f0130
belissa28 <41009713+belissa28@users.noreply.github.com>
2024-06-07 01:49:02 -0400

    Added text and a picture to my home page.

--- BelissaMobileApp/HomeView.swift
+++ BelissaMobileApp/HomeView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color(.black)
+                .ignoresSafeArea()
+           
+            VStack{
+            Text("Welcome!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.white)
+                .multilineTextAlignment(.center)
+                Image("Belissacomputer")
+                    .resizable()
+                    .cornerRadius(15)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.all)
+            Text("Hi there and welcome to my mobile webpage! I'm Belissa a proficient software engineer possessing comprehensive full-stack expertise, with a keen focus on frontend development and cybersecurity.")
+                    .foregroundColor(Color.white)
+            
+                
+            }
+        }
+        
     }
+    
+    
+    
+    
+    
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
