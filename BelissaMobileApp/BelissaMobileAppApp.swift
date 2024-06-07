2d7c59a16a669da8c7edd77cf3272be2e720cf74
belissa28 <41009713+belissa28@users.noreply.github.com>
2024-06-07 01:28:16 -0400

    Initial Commit

---
+++ BelissaMobileApp/BelissaMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BelissaMobileAppApp.swift
+//  BelissaMobileApp
+//
+//  Created by Belissa on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BelissaMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
